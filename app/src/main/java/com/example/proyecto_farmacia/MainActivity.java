1946fea2c3b0d00f1e55202253704fdcada12c91
Izhar Yahir <56055831+Izhar7QR@users.noreply.github.com>
2023-02-26 16:28:52 -0600

    comprobacion firebase

--- app/src/main/java/com/example/proyecto_farmacia/MainActivity.java
+++ app/src/main/java/com/example/proyecto_farmacia/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.proyecto_farmacia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundle = new Bundle();
+        bundle.putString("mesasge", "Integracion de Firebase");
+        mFirebaseAnalytics.logEvent("InitScreen", bundle);
     }
 }
