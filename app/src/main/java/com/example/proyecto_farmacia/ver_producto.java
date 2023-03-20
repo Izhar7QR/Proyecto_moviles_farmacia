ea891be788cfca5eff9267816ca89b8757615954
Izhar Yahir <izharyhp02@gmail.com>
2023-03-20 17:09:34 -0600

    format

--- app/src/main/java/com/example/proyecto_farmacia/ver_producto.java
+++ app/src/main/java/com/example/proyecto_farmacia/ver_producto.java
@@ -8,13 +8,6 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ver_producto extends AppCompatActivity {
-    public static final String stringNombres="names";
-    public static final String stringSirve="names";
-    public static final String stringefectos="names";
-    public static final String stringGraves="names";
-    public static final String stringUso="names";
-    public static final String stringUsar="names";
-    //Bundle stringUser = getIntent().getExtras();
     private TextView nombreProducto, SirveP, comoUsar, otroUsos, efectos, efectosGraves;
 
 
@@ -31,13 +24,6 @@ public class ver_producto extends AppCompatActivity {
         efectosGraves = findViewById(R.id.tv_efectoSecundarioGrave);
 
         ArrayList<String> lista = (ArrayList<String>) getIntent().getStringArrayListExtra("miLista");
-/*
-        String datos1 = getIntent().getStringExtra(stringNombres);
-        String datos2 = getIntent().getStringExtra(stringSirve);
-        String datos3 = getIntent().getStringExtra(stringefectos);
-        String datos4 = getIntent().getStringExtra(stringGraves);
-        String datos5 = getIntent().getStringExtra(stringUso);
-        String datos6 = getIntent().getStringExtra(stringUsar);*/
 
         nombreProducto.setText(lista.get(0));
         SirveP.setText(lista.get(1));
