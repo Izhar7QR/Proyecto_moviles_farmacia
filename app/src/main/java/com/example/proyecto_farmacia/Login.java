e4b09c98f0e26a381233f2331228b4978a77edfe
Cesar3333 <78836424+Cesar3333@users.noreply.github.com>
2023-03-20 14:09:08 -0600

    Verificacion de correo y parametros de contraseña y guardar en Firebase
    Se conecto a la base de Firebase y el los correo se valido que sea un correo y la contraseña minomo 8 caracteres

--- app/src/main/java/com/example/proyecto_farmacia/Login.java
+++ app/src/main/java/com/example/proyecto_farmacia/Login.java
@@ -1,35 +1,48 @@
 package com.example.proyecto_farmacia;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 
 public class Login extends AppCompatActivity {
 
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Button loginButton;
-    private Button signUpButton;
-    private ImageView logoImageView;
-    private ImageButton googleSignInButton;
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button loginButton;
+    Button signUpButton;
+    ImageView logoImageView;
+    FirebaseAuth mAuth;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
         // Referencias a los elementos del layout
         logoImageView = findViewById(R.id.logoImageView);
@@ -37,10 +50,13 @@ public class Login extends AppCompatActivity {
         editTextPassword = findViewById(R.id.editTextTextPassword);
         loginButton = findViewById(R.id.loginButton);
         signUpButton = findViewById(R.id.signUpButton);
-        googleSignInButton = findViewById(R.id.googleSignInButton);
-
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
         // Configuración de los botones
+
+
         loginButton.setOnClickListener(new View.OnClickListener() {
+
 
             public void onClick(View view) {
                 // Obtener las credenciales introduces
@@ -64,21 +80,13 @@ public class Login extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //
+                // Abrir la actividad de registro
                 Intent intent = new Intent(Login.this, SingUp.class);
                 startActivity(intent);
             }
-
         });
 
 
-
-        googleSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // Name, email address, and profile photo Url
@@ -94,7 +102,8 @@ public class Login extends AppCompatActivity {
             // FirebaseUser.getIdToken() instead.
             String uid = user.getUid();
         }
+
     }
+
+
 }
-
-
--- app/src/main/java/com/example/proyecto_farmacia/SingUp.java
+++ app/src/main/java/com/example/proyecto_farmacia/SingUp.java
@@ -6,55 +6,68 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
 
 public class SingUp extends AppCompatActivity {
-    private EditText usernameEditText;
-    private EditText passwordEditText;
-    private EditText confirmPasswordEditText;
-    private FirebaseAuth mAuth;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    EditText confirmPasswordEditText;
+    FirebaseAuth mAuth;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
         mAuth = FirebaseAuth.getInstance();
 
+
         // Obtenemos los elementos de la interfaz de usuario
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         Button registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
                 String confirmPassword = confirmPasswordEditText.getText().toString();
 
-                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
-                    Toast.makeText(getApplicationContext(), "Por favor complete todos los campos", Toast.LENGTH_SHORT).show();
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(username).matches()) {
+                    Toast.makeText(getApplicationContext(), "Por favor ingrese un correo electrónico válido", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+
                 if (!password.equals(confirmPassword)) {
                     Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                    // Validar contraseña
+                if (password.length() < 8) {
+                    Toast.makeText(getApplicationContext(), "La contraseña debe tener al menos 8 caracteres", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -66,6 +79,10 @@ public class SingUp extends AppCompatActivity {
             }
         });
     }
+
+
+
+
     private void registrarUsuario(String username, String password) {
         mAuth.createUserWithEmailAndPassword(username, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -80,13 +97,55 @@ public class SingUp extends AppCompatActivity {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
                             Toast.makeText(SingUp.this, "Authentication failed.", Toast.LENGTH_SHORT);
+
                         }
+                        mAuth.createUserWithEmailAndPassword(username, password)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                        if (task.isSuccessful()) {
+
+                                            Toast.makeText(SingUp.this, "Account created:.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Toast.makeText(SingUp.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+                                });
                     }
+
                 });
 
         // En este ejemplo, simplemente mostramos un mensaje de éxito
         Toast.makeText(getApplicationContext(), "Usuario registrado exitosamente", Toast.LENGTH_SHORT).show();
+        FirebaseAuth.getInstance().signOut();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+        } else {
+            // No user is signed in
+        }
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+
+
     }
+
 
 
 
